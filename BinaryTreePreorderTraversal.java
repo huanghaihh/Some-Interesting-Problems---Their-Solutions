652d06e511dca70456ff41025d24a5f7ef0cb5ac
Hai Huang <chasedorothy@hotmail.com>
2015-08-28 16:09:47 -0400

    add another solution

--- BinaryTreePreorderTraversal.java
+++ BinaryTreePreorderTraversal.java
@@ -34,3 +34,19 @@ public class BinaryTreePreorderTraversal{
     	return list;
     }
 }
+
+public class BinaryTreePreorderTraversal{
+    public List<Integer> preorderTraversal(TreeNode root){
+        List<Integer> list = new ArrayList<Integer>();
+        if(root == null) return list;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            root = stack.pop();
+            lsit.add(root.val);
+            if(root.right != null) stack.push(root.right);
+            if(root.left != null) stack.push(root.left);
+        }
+        return list;
+    }
+}
